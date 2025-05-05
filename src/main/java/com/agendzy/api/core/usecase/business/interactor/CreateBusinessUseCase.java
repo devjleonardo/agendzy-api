9c9c78e9cca3dadc0f4d86a75491bb2c7ec1dfe7
devjleonardo <leonardo_cordeirobahia@hotmail.com>
2025-05-05 00:00:07 -0300

    Removendo endpoint de cadastrar diretamente todos dados do business

--- src/main/java/com/agendzy/api/core/usecase/business/interactor/CreateBusinessUseCase.java
+++ src/main/java/com/agendzy/api/core/usecase/business/interactor/CreateBusinessUseCase.java
@@ -1,142 +1,142 @@
-package com.agendzy.api.core.usecase.business.interactor;
-
-import com.agendzy.api.core.domain.business.Business;
-import com.agendzy.api.core.domain.business.collaborator.Collaborator;
-import com.agendzy.api.core.domain.business.collaborator.CollaboratorRole;
-import com.agendzy.api.core.domain.business.openinghours.BusinessOpeningHours;
-import com.agendzy.api.core.domain.business.phone.BusinessPhone;
-import com.agendzy.api.core.domain.business.phone.PhoneType;
-import com.agendzy.api.core.domain.business.professional.Professional;
-import com.agendzy.api.core.domain.business.service.BusinessService;
-import com.agendzy.api.core.domain.common.User;
-import com.agendzy.api.core.gateway.common.SaveGateway;
-import com.agendzy.api.core.usecase.business.boundary.input.data.openinghours.BusinessOpeningHoursInput;
-import com.agendzy.api.core.usecase.business.boundary.input.data.CreateBusinessInput;
-import com.agendzy.api.core.usecase.business.boundary.input.data.service.ServiceInput;
-import com.agendzy.api.core.usecase.business.boundary.output.BusinessCreatedOutput;
-import com.agendzy.api.core.usecase.common.boundary.output.data.outputresponse.OutputResponse;
-import com.agendzy.api.core.usecase.common.interactor.CreateUserUseCase;
-import com.agendzy.api.util.LogUtil;
-import com.agendzy.api.util.TimeMappingUtil;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.Duration;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import static com.agendzy.api.core.usecase.common.boundary.output.data.outputresponse.OutputResponseFactory.success;
-
-@Service
-@RequiredArgsConstructor
-public class CreateBusinessUseCase {
-
-    private final CreateUserUseCase createUser;
-    private final SaveGateway<Business> businessSave;
-    private final SaveGateway<Collaborator> collaboratorSave;
-
-    @Transactional
-    public OutputResponse<BusinessCreatedOutput> execute(CreateBusinessInput input) {
-        try {
-            User savedUser = createUser.execute(input.getUser());
-            Business savedBusiness = saveBusinessAndOwner(input, savedUser);
-            return success(BusinessCreatedOutput.of(savedBusiness));
-        } catch (Exception e) {
-            LogUtil.logError("Unexpected error while creating business.", e);
-            throw e;
-        }
-    }
-
-    private Business saveBusinessAndOwner(CreateBusinessInput input, User user) {
-        Business business = buildBusiness(input, user);
-        Business savedBusiness = businessSave.execute(business).getData();
-
-        Collaborator owner = buildOwnerCollaborator(user, savedBusiness);
-        Collaborator savedOwner = collaboratorSave.execute(owner).getData();
-
-        savedBusiness.setOwner(savedOwner);
-
-        return businessSave.execute(savedBusiness).getData();
-    }
-
-    private Business buildBusiness(CreateBusinessInput input, User ownerUser) {
-        Business business = new Business();
-        business.setName(input.getName());
-        business.setSegment(input.getSegment());
-        business.setLocation(input.getLocation());
-        business.setTeamSize(input.getTeamSize());
-
-        if (input.getServices() != null) {
-            business.setServices(mapServices(input, business));
-        }
-
-        if (input.getOpeningHours() != null) {
-            business.setOpeningHours(mapOpeningHours(input.getOpeningHours(), business));
-        }
-
-        if (ownerUser.getPhoneNumber() != null && !ownerUser.getPhoneNumber().isBlank()) {
-            business.addPhone(buildOwnerPhone(ownerUser, business));
-        }
-
-        return business;
-    }
-
-    private BusinessPhone buildOwnerPhone(User owner, Business business) {
-        BusinessPhone phone = new BusinessPhone();
-        phone.setPhoneNumber(owner.getPhoneNumber());
-        phone.setType(PhoneType.WHATSAPP);
-        phone.setBusiness(business);
-        return phone;
-    }
-
-    private Collaborator buildOwnerCollaborator(User user, Business business) {
-        Professional collaborator = new Professional();
-        collaborator.setName(user.getFullName());
-        collaborator.setNickname(extractNickname(user.getFullName()));
-        collaborator.setRole(CollaboratorRole.OWNER);
-        collaborator.setUser(user);
-        collaborator.setBusiness(business);
-        return collaborator;
-    }
-
-    private Set<BusinessService> mapServices(CreateBusinessInput input, Business business) {
-        return input.getServices().stream()
-            .filter(this::isValidServiceInput)
-            .map(serviceInput -> {
-                BusinessService service = new BusinessService();
-                service.setName(serviceInput.getName());
-                service.setPrice(serviceInput.getPrice());
-                service.setDuration(Duration.ofMinutes(serviceInput.getDurationInMinutes()));
-                service.setBusiness(business);
-                return service;
-            })
-            .collect(Collectors.toSet());
-    }
-
-    private boolean isValidServiceInput(ServiceInput serviceInput) {
-        return serviceInput.getName() != null
-            && serviceInput.getPrice() != null
-            && serviceInput.getDurationInMinutes() != null;
-    }
-
-    private Set<BusinessOpeningHours> mapOpeningHours(List<BusinessOpeningHoursInput> inputs, Business business) {
-        return inputs.stream()
-            .map(input -> {
-                BusinessOpeningHours hour = new BusinessOpeningHours();
-                hour.setBusiness(business);
-                hour.setDayOfWeek(TimeMappingUtil.toDayOfWeek(input.getDayOfWeek()));
-                hour.setStartTime(TimeMappingUtil.toLocalTime(input.getStartTime()));
-                hour.setEndTime(TimeMappingUtil.toLocalTime(input.getEndTime()));
-                hour.setEnabled(input.isEnabled());
-                return hour;
-            })
-            .collect(Collectors.toSet());
-    }
-
-    private String extractNickname(String fullName) {
-        return fullName.contains(" ") ? fullName.split(" ")[0] : fullName;
-    }
-
-}
+//package com.agendzy.api.core.usecase.business.interactor;
+//
+//import com.agendzy.api.core.domain.business.Business;
+//import com.agendzy.api.core.domain.business.collaborator.Collaborator;
+//import com.agendzy.api.core.domain.business.collaborator.CollaboratorRole;
+//import com.agendzy.api.core.domain.business.openinghours.BusinessOpeningHours;
+//import com.agendzy.api.core.domain.business.phone.BusinessPhone;
+//import com.agendzy.api.core.domain.business.phone.PhoneType;
+//import com.agendzy.api.core.domain.business.professional.Professional;
+//import com.agendzy.api.core.domain.business.service.BusinessService;
+//import com.agendzy.api.core.domain.common.User;
+//import com.agendzy.api.core.gateway.common.SaveGateway;
+//import com.agendzy.api.core.usecase.business.boundary.input.data.openinghours.BusinessOpeningHoursInput;
+//import com.agendzy.api.core.usecase.business.boundary.input.data.CreateBusinessInput;
+//import com.agendzy.api.core.usecase.business.boundary.input.data.service.ServiceInput;
+//import com.agendzy.api.core.usecase.business.boundary.output.BusinessCreatedOutput;
+//import com.agendzy.api.core.usecase.common.boundary.output.data.outputresponse.OutputResponse;
+//import com.agendzy.api.core.usecase.common.interactor.CreateUserUseCase;
+//import com.agendzy.api.util.LogUtil;
+//import com.agendzy.api.util.TimeMappingUtil;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.time.Duration;
+//import java.util.List;
+//import java.util.Set;
+//import java.util.stream.Collectors;
+//
+//import static com.agendzy.api.core.usecase.common.boundary.output.data.outputresponse.OutputResponseFactory.success;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class CreateBusinessUseCase {
+//
+//    private final CreateUserUseCase createUser;
+//    private final SaveGateway<Business> businessSave;
+//    private final SaveGateway<Collaborator> collaboratorSave;
+//
+//    @Transactional
+//    public OutputResponse<BusinessCreatedOutput> execute(CreateBusinessInput input) {
+//        try {
+//            User savedUser = createUser.execute(input.getUser());
+//            Business savedBusiness = saveBusinessAndOwner(input, savedUser);
+//            return success(BusinessCreatedOutput.of(savedBusiness));
+//        } catch (Exception e) {
+//            LogUtil.logError("Unexpected error while creating business.", e);
+//            throw e;
+//        }
+//    }
+//
+//    private Business saveBusinessAndOwner(CreateBusinessInput input, User user) {
+//        Business business = buildBusiness(input, user);
+//        Business savedBusiness = businessSave.execute(business).getData();
+//
+//        Collaborator owner = buildOwnerCollaborator(user, savedBusiness);
+//        Collaborator savedOwner = collaboratorSave.execute(owner).getData();
+//
+//        savedBusiness.setOwner(savedOwner);
+//
+//        return businessSave.execute(savedBusiness).getData();
+//    }
+//
+//    private Business buildBusiness(CreateBusinessInput input, User ownerUser) {
+//        Business business = new Business();
+//        business.setName(input.getName());
+//        business.setSegment(input.getSegment());
+//        business.setLocation(input.getLocation());
+//        business.setTeamSize(input.getTeamSize());
+//
+//        if (input.getServices() != null) {
+//            business.setServices(mapServices(input, business));
+//        }
+//
+//        if (input.getOpeningHours() != null) {
+//            business.setOpeningHours(mapOpeningHours(input.getOpeningHours(), business));
+//        }
+//
+//        if (ownerUser.getPhoneNumber() != null && !ownerUser.getPhoneNumber().isBlank()) {
+//            business.addPhone(buildOwnerPhone(ownerUser, business));
+//        }
+//
+//        return business;
+//    }
+//
+//    private BusinessPhone buildOwnerPhone(User owner, Business business) {
+//        BusinessPhone phone = new BusinessPhone();
+//        phone.setPhoneNumber(owner.getPhoneNumber());
+//        phone.setType(PhoneType.WHATSAPP);
+//        phone.setBusiness(business);
+//        return phone;
+//    }
+//
+//    private Collaborator buildOwnerCollaborator(User user, Business business) {
+//        Professional collaborator = new Professional();
+//        collaborator.setName(user.getFullName());
+//        collaborator.setNickname(extractNickname(user.getFullName()));
+//        collaborator.setRole(CollaboratorRole.OWNER);
+//        collaborator.setUser(user);
+//        collaborator.setBusiness(business);
+//        return collaborator;
+//    }
+//
+//    private Set<BusinessService> mapServices(CreateBusinessInput input, Business business) {
+//        return input.getServices().stream()
+//            .filter(this::isValidServiceInput)
+//            .map(serviceInput -> {
+//                BusinessService service = new BusinessService();
+//                service.setName(serviceInput.getName());
+//                service.setPrice(serviceInput.getPrice());
+//                service.setDuration(Duration.ofMinutes(serviceInput.getDurationInMinutes()));
+//                service.setBusiness(business);
+//                return service;
+//            })
+//            .collect(Collectors.toSet());
+//    }
+//
+//    private boolean isValidServiceInput(ServiceInput serviceInput) {
+//        return serviceInput.getName() != null
+//            && serviceInput.getPrice() != null
+//            && serviceInput.getDurationInMinutes() != null;
+//    }
+//
+//    private Set<BusinessOpeningHours> mapOpeningHours(List<BusinessOpeningHoursInput> inputs, Business business) {
+//        return inputs.stream()
+//            .map(input -> {
+//                BusinessOpeningHours hour = new BusinessOpeningHours();
+//                hour.setBusiness(business);
+//                hour.setDayOfWeek(TimeMappingUtil.toDayOfWeek(input.getDayOfWeek()));
+//                hour.setStartTime(TimeMappingUtil.toLocalTime(input.getStartTime()));
+//                hour.setEndTime(TimeMappingUtil.toLocalTime(input.getEndTime()));
+//                hour.setEnabled(input.isEnabled());
+//                return hour;
+//            })
+//            .collect(Collectors.toSet());
+//    }
+//
+//    private String extractNickname(String fullName) {
+//        return fullName.contains(" ") ? fullName.split(" ")[0] : fullName;
+//    }
+//
+//}
--- src/main/java/com/agendzy/api/entrypoint/http/resource/business/BusinessResource.java
+++ src/main/java/com/agendzy/api/entrypoint/http/resource/business/BusinessResource.java
@@ -1,13 +1,6 @@
 package com.agendzy.api.entrypoint.http.resource.business;
 
-import com.agendzy.api.core.usecase.business.boundary.input.data.CreateBusinessInput;
-import com.agendzy.api.core.usecase.business.interactor.CreateBusinessUseCase;
-import com.agendzy.api.entrypoint.http.HandlingResponse;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class BusinessResource {
 
-    private final CreateBusinessUseCase createBusinessUseCase;
-
-    @PostMapping
-    public ResponseEntity<Object> createBusinessWithOwner(@RequestBody @Valid CreateBusinessInput input) {
-        return HandlingResponse.execute(createBusinessUseCase.execute(input));
-    }
+//    private final CreateBusinessUseCase createBusinessUseCase;
+//
+//    @PostMapping
+//    public ResponseEntity<Object> createBusinessWithOwner(@RequestBody @Valid CreateBusinessInput input) {
+//        return HandlingResponse.execute(createBusinessUseCase.execute(input));
+//    }
 
 }
